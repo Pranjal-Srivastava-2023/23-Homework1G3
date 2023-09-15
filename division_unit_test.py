f87bf118a87d919e77b2b1b8a8bc540939fe152c
tirthbha <143649367+tirthbha@users.noreply.github.com>
2023-09-15 14:01:25 -0400

    Simpler way to do division using a conditional expression!

--- division_unit_test.py
+++ division_unit_test.py
@@ -1,18 +1,14 @@
-# Define a function called 'division' that takes two arguments, 'x' and 'y'
+# This code has the same functionality as your original code but in a more simpler way.
+
 def division(x, y):
-    # Check if 'y' is equal to 0
-    if y == 0:
-        # If 'y' is 0, return the string "ERROR" (division by zero is undefined)
-        return "ERROR"
-    else:
-        # If 'y' is not 0, return the result of 'x' divided by 'y'
-        return x / y
+    return x / y if y != 0 else "ERROR"
+result = division(6, 2)
+print(result)
+    
+#output=3.0
 
-# Assert statements are used to test the 'division' function
-# They check if the function behaves as expected for specific inputs
+    # Here x / y is calculated when y is not equal to 0 (i.e., when division is possible).
+   # y != 0 : If y is not equal to 0, it returns the result of x / y, which is the division result. (using conditional statement)
+   #  If y is equal to 0, it returns "ERROR" to indicate a division by zero error.
 
-# Assert that division(2, 1) should return 2 and so on...
-assert division(2, 1) == 2
-assert division(0, 5) == 0
-assert division(1, 2) == 0.5
-assert division(1, 0) == "ERROR"
+
